c6a061fdd62d354ae96257e1ff038c3179553289
paula <pflogar0401@g.educaand.es>
2022-12-11 19:20:50 +0100

    Terminado y comentado

--- dist/index.f8a5f574.js
+++ dist/index.f8a5f574.js
@@ -1,8 +1,8 @@
 const colorSwitch = document.querySelector('button[type="button"]');
 var imagen = document.getElementById("imagenTema");
 function cambiarTema() {
-    if (document.documentElement.getAttribute("tema") == "dia") document.documentElement.setAttribute("tema", "noche");
-    else document.documentElement.setAttribute("tema", "dia");
+    if (document.documentElement.getAttribute("tema") == "claro") document.documentElement.setAttribute("tema", "oscuro");
+    else document.documentElement.setAttribute("tema", "claro");
 }
 colorSwitch.addEventListener("click", cambiarTema);
 
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -2,11 +2,11 @@ const colorSwitch = document.querySelector('button[type="button"]');
 var imagen = document.getElementById('imagenTema');
 
 function cambiarTema(){
-    if (document.documentElement.getAttribute("tema") == "dia"){
-        document.documentElement.setAttribute("tema","noche");
+    if (document.documentElement.getAttribute("tema") == "claro"){
+        document.documentElement.setAttribute("tema","oscuro");
     }
     else{
-        document.documentElement.setAttribute("tema","dia");
+        document.documentElement.setAttribute("tema","claro");
     }
 }
 
